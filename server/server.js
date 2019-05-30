eaf93079d9fcb965b6ef1625150481d936488e34
roey <rliba1990@gmail.com>
2019-05-30 17:47:47 +0300

    server update with db

--- server/server.js
+++ server/server.js
@@ -1,27 +1,24 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000;
+const express = require('express');
+var cors = require('cors');
+
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 //Local
-
 const yargs = require('yargs');
-
 const arguments = yargs.argv._
-
-console.log(arguments)
-
+const app = express();
+app.use(cors());
 app.use(express.static('public'));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+console.log(arguments)
 
 //connect mongoDB
-const url = "mongodb://localhost:27017/newDb";
+const url = "mongodb://localhost:27017/IGDB";
 mongoose.connect(url, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -31,41 +28,41 @@ db.once('open', () => {
 
 //Define a schema
 const Schema = mongoose.Schema;
-const SomeModelSchema = new Schema({
-    name: String,
-    last: String,
-    age: Number,
-    date: Date
+const reviewScheme = new Schema({
+    authorName: String,
+    authorLast: String,
+    img: String,
+    gameName: String,
+    rating: Number,
+    date: Date,
+    reviewDecription: String,
+    id: mongoose.ObjectId
+});
+const usersScheme = new Schema({
+    userName: String,
+    pass: String,
 });
 
 
-//Schema types can be:
 
-// String
-// Number
-// Date
-// Buffer - https://hackernoon.com/https-medium-com-amanhimself-converting-a-buffer-to-json-and-utf8-strings-in-nodejs-2150b1e3de57
-// Boolean
-// Mixed - https://mongoosejs.com/docs/schematypes.html#mixed
-// ObjectId
-// Array
-// Decimal128
-// Map
+// for (i = 0; i < 10; i++) {
+//     //Schema types can be:
 
-// create collection (model) with it's schema
-const SomeModel = mongoose.model('myModel', SomeModelSchema);
+//     // String
+//     // Number
+//     // Date
+//     // Buffer - https://hackernoon.com/https-medium-com-amanhimself-converting-a-buffer-to-json-and-utf8-strings-in-nodejs-2150b1e3de57
+//     // Boolean
+//     // Mixed - https://mongoosejs.com/docs/schematypes.html#mixed
+//     // ObjectId
+//     // Array
+//     // Decimal128
+//     // Map
+// }
 
-// Create an instance of model SomeModel
-var awesome_instance = new SomeModel({ name: 'awesome', last: "bla", age: 12 });
 
-// Save the new model instance, passing a callback
-awesome_instance.save(function (err) {
-    if (err) return handleError(err);
-    console.log('saved')
-});
-
-// CRUD
-// https://mongoosejs.com/docs/queries.html
+// // CRUD
+// // https://mongoosejs.com/docs/queries.html
 
 // SomeModel.find({ name: 'awesome' }, (err, docs) => {
 //     if (err) throw err;
@@ -77,19 +74,30 @@ awesome_instance.save(function (err) {
 
 
 
-// if (arguments == 'create') {
-//     MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
-//         if (err) throw err;
-//         var dbo = db.db("nodechef-mongo");
-//         dbo.createCollection("customers", function (err, res) {
-//             if (err) throw err;
-//             console.log("Collection created!");
-//             db.close();
-//             res.send({ data: "Collection created!" })
-//         });
-//     });
-// }
+if (arguments == 'createReviews') {
+    // create collection (model) with it's schema
+    const reviewsModel = mongoose.model('reviews', reviewScheme);
 
+    // Create an instance of model SomeModel
+    var reviewInstance = new reviewsModel({ authorName: 'awesome', authorLast: "bla", img: "", date: Date.now(), gameName: 'doom3', rating: 3.6, reviewDecription: 'yesh li tsav she korim lo yehoshua' });
+
+    // Save the new model instance, passing a callback
+    reviewInstance.save(function (err) {
+
+    });
+}
+if (arguments == 'createUsers') {
+    // create collection (model) with it's schema
+    const usersModel = mongoose.model('users', usersScheme);
+
+    // Create an instance of model SomeModel
+    var usersInstance = new usersModel({ userName: 'moshe', pass: 'asd' });
+
+    // Save the new model instance, passing a callback
+    usersInstance.save(function (err) {
+
+    });
+}
 
 // if (arguments == 'setNew') {
 
@@ -154,4 +162,25 @@ awesome_instance.save(function (err) {
 //     });
 // })
 
-app.listen(port, () => console.log(`server listening on port ${port}!!!`))
+app.get('/', (req, res) => {
+
+    res.send('<h1>I like Big Buttssss</h1>')
+})
+
+app.get('/getMeMyAc', (req, res) => {
+    setTimeout(() => {
+        res.send({
+            name: 'Arnold',
+            last: 'schwartzeniggger',
+            link: 'https://i.pinimg.com/originals/99/4d/85/994d8552520d84e068a0b513fddf1b13.jpg'
+        })
+    }, 2000
+    )
+})
+
+
+
+//#region port
+let port = process.env.port || 4000;
+app.listen(port, function () { console.log("server ur on", port) })
+//#endregion
