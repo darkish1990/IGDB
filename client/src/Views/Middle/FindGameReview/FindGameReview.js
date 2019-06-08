cacd07fa56027204f40c2c57f5c3ae29c02c1b84
roey <Rliba1990@gmail.com>
2019-06-08 15:55:12 +0300

    divide to sub components and remove news api idea from app

---
+++ client/src/Views/Middle/FindGameReview/FindGameReview.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './FindGameReview.css';
+
+function FindGameReview() {
+
+  return (
+    <div className="FindGameReview" >
+         <div className="InjectedContent" >
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+        </div>
+    </div>
+  );
+}
+
+
+
+export default FindGameReview;
