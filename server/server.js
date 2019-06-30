6fd2d7bfbe61d5087b618f79a25d694c8ea96aa4
roey <rliba1990@gmail.com>
2019-06-30 19:59:49 +0300

    add create reviews to db on server start with yargs for 3 instances

--- server/server.js
+++ server/server.js
@@ -55,25 +55,63 @@ if (arguments.includes('createReviews')) {
 
 
     // Create an instance of model SomeModel
-    var reviewInstance = new reviewsModel({ authorName: 'awesome', img: "https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?cs=srgb&dl=beautiful-beauty-blue-414612.jpg&fm=jpg", date: Date.now(), gameName: 'doom3', rating: 3.6, reviewDescription: 'yesh li tsav she korim lo yehoshua' });
+    var reviewInstance = new reviewsModel({
+        authorName: 'awesome',
+        img: "https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?cs=srgb&dl=beautiful-beauty-blue-414612.jpg&fm=jpg",
+        date: Date.now(), gameName: 'something', rating: 3.6, reviewDescription: 'yesh li tsav she korim lo yehoshua'
+    });
     console.log(reviewInstance)
     // Save the new model instance, passing a callback
     reviewInstance.save(function (err) {
 
     });
+    /* 1 */
+    reviewInstance = new reviewsModel({
+        authorName: "awesome",
+        img: "https://d2skuhm0vrry40.cloudfront.net/2017/articles/2017-10-23-15-36/GTA_5_Online_LEAK_Duke_O_Death_DLC_update_coming_soon_605675.jpg/EG11/resize/300x-1/quality/75/format/jpg",
+        date: "2019-06-30T10:39:15.918Z",
+        gameName: "GTA5",
+        rating: 4.8,
+        reviewDescription: "bla bla GTA 5"
+
+    })
+    reviewInstance.save(function (err) { });
+    /* 2 */
+    reviewInstance = new reviewsModel({
+        authorName: "roey",
+        img: "https://hb.imgix.net/fb1a527bf84329358b1732e0432e189a047117f2.jpg?auto=compress,format&fit=crop&h=353&w=616&s=de8581ca3e0437aed022c72a1f8e99f6",
+        date: "2019-06-30T13:28:37.048Z",
+        gameName: "Doom3",
+        rating: 1.3,
+        reviewDescription: "Killing monsters and such \nmichael and belete look at aviv he is waiting for ya all \n\nsup sup u guys ",
+    })
+    reviewInstance.save(function (err) { });
+
+    /* 3 */
+    reviewInstance = new reviewsModel({
+        authorName: "roey",
+        img: "https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/Dark_souls_3_cover_art.jpg/220px-Dark_souls_3_cover_art.jpg",
+        date: "2019-06-30T13:31:44.006Z",
+        gameName: "Dark Souls 3",
+        rating: null,
+        reviewDescription: "The Best Game eveerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr",
+    })
+
+    reviewInstance.save(function (err) { });
+
 }
-if (arguments.includes('createUsers')) {
-    // create collection (model) with it's schema
-    const usersModel = mongoose.model('users', usersScheme);
+// if (arguments.includes('createUsers')) {
+//     // create collection (model) with it's schema
+//     const usersModel = mongoose.model('users', usersScheme);
 
-    // Create an instance of model SomeModel
-    var usersInstance = new usersModel({ userName: 'moshe', pass: 'asd' });
+//     // Create an instance of model SomeModel
+//     var usersInstance = new usersModel({ userName: 'moshe', pass: 'asd' });
 
-    // Save the new model instance, passing a callback
-    usersInstance.save(function (err) {
+//     // Save the new model instance, passing a callback
+//     usersInstance.save(function (err) {
 
-    });
-}
+//     });
+// }
 
 
 
