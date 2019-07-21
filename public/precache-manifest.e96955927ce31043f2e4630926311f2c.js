845c57b20fa385040da94b1a07091c559e679dfc
roey <rliba1990@gmail.com>
2019-07-21 14:17:32 +0300

    ט

---
+++ public/precache-manifest.e96955927ce31043f2e4630926311f2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a01cec0243b3b59a8cf2a15327e4e213",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbfd0230df758238a222",
+    "url": "/static/css/main.fbbce637.chunk.css"
+  },
+  {
+    "revision": "6fa9952e399ecf889c11",
+    "url": "/static/js/2.4e590716.chunk.js"
+  },
+  {
+    "revision": "fbfd0230df758238a222",
+    "url": "/static/js/main.7046ff77.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
