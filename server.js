05702f4148deda3c9a7e8af8bf0c17bf282aec24
roey <Rliba1990@gmail.com>
2019-08-29 23:13:07 +0300

    loibabe

--- server.js
+++ server.js
@@ -151,7 +151,7 @@ app.post("/Api/AddReview", (req, res) => {
   });
 });
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "/index.html"), function(err) {
+  res.sendFile(path.join(__dirname, "public/index.html"), function(err) {
     if (err) {
       res.status(500).send(err);
     }
